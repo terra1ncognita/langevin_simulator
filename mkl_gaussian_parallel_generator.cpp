e60b6d4e084abf3fc5fab58b7af83689808569aa
terra1ncognita <ivan.gonchar@phystech.edu>
2020-10-31 19:53:34 +0300

    added log print to generator

--- mkl_gaussian_parallel_generator.cpp
+++ mkl_gaussian_parallel_generator.cpp
@@ -7,20 +7,21 @@
 
 
 MklGaussianParallelGenerator::MklGaussianParallelGenerator(double mean, double stDeviation, std::size_t bufferSize, unsigned threadNum)
-	:_mean{ mean }, _stDeviation{ stDeviation }, _bufferSize{bufferSize},_threadNum{threadNum}
+	:_mean{ mean }, _stDeviation{ stDeviation }, _bufferSize{bufferSize}, _threadNum{threadNum}
 {
-	///////////////// If reproducibility from launch to launch is required, then seed is const, else seed must be random
-	srand(time(NULL));
-	for (unsigned i = 0; i < threadNum; i++) {
-		MKL_UINT seed = static_cast<MKL_UINT>(rand());
-		_streamWrappers.emplace_back(VSL_BRNG_MT2203 + i, seed);
-	}
-
 	_nPerThread = _bufferSize / _threadNum;
 	if (_bufferSize != _nPerThread * _threadNum) {
 		throw std::logic_error{ "buffsize must be multiple of number of threads" };
 	}
 	_buffer.resize(_bufferSize);
+
+	///////////////// If reproducibility from launch to launch is required, then seed is const, else seed must be random
+	srand(time(NULL));
+	for (unsigned i = 0; i < threadNum; i++) {
+		MKL_UINT seed = static_cast<MKL_UINT>(rand());
+		_streamWrappers.emplace_back(VSL_BRNG_MT2203 + i, seed);
+		std::cout << "Added generator thread with seed " << seed << std::endl;
+	}
 }
 
 void MklGaussianParallelGenerator::generateNumbers()
@@ -28,7 +29,7 @@ void MklGaussianParallelGenerator::generateNumbers()
 	//// Strange that it works without shared! check this out.
 #pragma omp parallel num_threads(_threadNum) default(none)
 	{
-		const std::size_t threadId=omp_get_thread_num();
+		const std::size_t threadId = omp_get_thread_num();
 		const std::size_t begin = _nPerThread * threadId;
 		const auto generationResult = vdRngGaussian(
 			VSL_RNG_METHOD_GAUSSIAN_ICDF, _streamWrappers.at(threadId).get(),
