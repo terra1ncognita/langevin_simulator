c1fbccdf5efb8b8b681ef5868abab4c031d40293
Ivan Gonchar <ivan.gonchar@phystech.edu>
2020-11-02 22:47:09 +0300

    Parameter tuning and paths

--- launcher.py
+++ launcher.py
@@ -2,8 +2,8 @@ import copy
 import itertools as it
 import json
 import os
-import subprocess
 from optparse import OptionParser, Values
+from socket import gethostname
 from typing import Dict, List
 
 
@@ -16,24 +16,6 @@ def get_option(options: Values, key: str):
     if not hasattr(options, key):
         raise ValueError(f"There is no {key} option")
     return getattr(options, key)
-
-
-def run_simulator(configs: List[str], options: Values) -> None:
-    """ Launches simulator langevin_simulator-binding with passing arguments -paramsfile, -taskfile, -nthreads.
-
-    :param configs: contains the names of the created configuration files
-    :param options: contains the value of the argument (-paramsfile, -taskfile, -nthreads).
-    """
-    nthreads = int(get_option(options, "nthreads"))
-    exe_file = os.path.join(get_option(options, "input_folder"), "langevin_simulator-binding.exe")
-    paramsfile = ";".join([os.path.join(get_option(options, "results_folder"), item) for item in configs])
-    taskfile = os.path.join(get_option(options, "input_folder"), get_option(options, "task_file_name"))
-
-    arg = f"{exe_file} -paramsfile {paramsfile} -taskfile {taskfile} -nthreads {nthreads - 1}"
-
-    simulator_exit_code = subprocess.call(arg)
-    if not simulator_exit_code:
-        raise RuntimeError(f"simulator returned exit code {simulator_exit_code}")
 
 
 def get_git_tag(path: str) -> str:
@@ -61,7 +43,13 @@ def apply_patch(conf: Dict, patch: Dict[str, str]):
 
 
 def create_configs(
-    task_file_name: str, results_folder: str, parent_config_filename: str, git_tag: str, counter_file: str,
+    task_filename: str,
+    configs_folder: str,
+    parent_config_filename: str,
+    git_tag: str,
+    counter_file: str,
+    results_folder: str,
+    machine_id: str,
 ):
     """Creates parent_config files based on the parent parent_config file and writes to them all possible
     combinations of parameter values from the job file. .
@@ -69,7 +57,7 @@ def create_configs(
     :return: a list with the names of the created files
     """
     list_configs = []
-    with open(task_file_name) as f:
+    with open(task_filename) as f:
         task = json.load(f)
     with open(parent_config_filename) as f:
         parent_config = json.load(f)
@@ -80,13 +68,25 @@ def create_configs(
         patches = (dict(zip(cross_scan_dict.keys(), comb)) for comb in it.product(*cross_scan_dict.values()))
         for patch in patches:
             count += 1
+            config_filename = f"{count}.json"
+            save_folder = os.path.join(results_folder, f"{count}_{machine_id}/")
+            if not os.path.exists(save_folder):
+                os.mkdir(save_folder)
 
             config = copy.deepcopy(parent_config)
             apply_patch(config["Configuration"], patch)
-            apply_patch(config, {"Configuration->gitHashShort": git_tag, "Configuration->Name": str(count)})
-
-            config_filename = f"{count}.json"
-            with open(os.path.join(results_folder, config_filename), "w") as f:
+            apply_patch(
+                config,
+                {
+                    "Configuration->gitHashShort": git_tag,
+                    "Configuration->Name": str(count),
+                    "Configuration->LoggerParameters->FilePath": save_folder,
+                    "Configuration->ParentConfigName": parent_config_filename,
+                    "Configuration->TaskFile": task_filename,
+                    "Configuration->ComputerID": machine_id,
+                },
+            )
+            with open(os.path.join(configs_folder, config_filename), "w") as f:
                 json.dump(config, f, sort_keys=True, indent=2)
             list_configs.append(config_filename)
 
@@ -110,8 +110,11 @@ def set_counter_file(filename: str, count: int) -> None:
         f.write(f"{count}\n")
 
 
-if __name__ == "__main__":
+def get_machine_id() -> str:
+    return gethostname()
 
+
+def main():
     parser = OptionParser()
 
     parser.add_option("-t", "--task_file_name")
@@ -119,6 +122,7 @@ if __name__ == "__main__":
     parser.add_option("-r", "--results_folder")
     parser.add_option("-g", "--git_path")
     parser.add_option("-c", "--counter_filename")
+    parser.add_option("-f", "--configs_folder")
 
     (options, args) = parser.parse_args()
 
@@ -127,14 +131,23 @@ if __name__ == "__main__":
     task_filename = get_option(options, "task_file_name")
     git_path = get_option(options, "git_path")
     counter_filename = get_option(options, "counter_filename")
+    configs_folder = get_option(options, "configs_folder")
+
+    machine_id = get_machine_id()
 
     config_files = create_configs(
-        task_file_name=task_filename,
+        task_filename=task_filename,
+        configs_folder=configs_folder,
         parent_config_filename=parent_config_filename,
-        results_folder=results_folder,
         git_tag=get_git_tag(git_path),
         counter_file=counter_filename,
+        results_folder=results_folder,
+        machine_id=machine_id,
     )
 
     create_params_file(os.path.join(results_folder, "sims.dat"), config_files)
     print("\n".join(config_files))
+
+
+if __name__ == "__main__":
+    main()
